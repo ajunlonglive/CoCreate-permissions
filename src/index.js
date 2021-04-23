a06a62235bc6cdb841d9b72d645be443124957a0
frankpagan <frank@cocreate.app>
2021-04-23 21:30:45 +0000

    fix: remove securityKey

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { CRUD_PERMISSION } = require('./constant')
 const url = require("url");
-const {ObjectID} = require("mongodb");
 
 /**
  * 
@@ -27,12 +26,11 @@ const {ObjectID} = require("mongodb");
  */
 
 class CoCreatePermission {
-	constructor(db_client) {
+	constructor() {
 		this.permissions = new Map();
-		this.dbClient = db_client
 	}
 	
-	setPermissionOfKey(apikey, permission) {
+	setPermissionObject(apikey, permission) {
 		this.permissions.set(apikey, permission)
 	}
 	
@@ -50,25 +48,77 @@ class CoCreatePermission {
 				//. if user_id/token => domain => colections => roles = true
 			//. merget roles collection and permission collection
 			
-			let permission = {};
-			if (this.dbClient) {
-				permission = await this.getPermissionOfapikey(apikey, organization_id);
-			}
+			let permission = await this.getPermissionObject(apikey, organization_id);
 
 			this.permissions.set(apikey, permission)
 			return permission
 		}
 	}
 	
+	//. overrride function
 	getParameters(action, data) {
-		const { apiKey, organization_id, collection } = data;
-		return {
-			apikey: apiKey,
-			organization_id,
-			key: 'collections',
-			key_value: collection,
-			type: action
-		}
+		return {};
+		// const { apiKey, organization_id, collection } = data;
+		// return {
+		// 	apikey: apiKey,
+		// 	organization_id,
+		// 	key: 'collections',
+		// 	key_value: collection,
+		// 	type: action
+		// }
+		
+	}
+	
+	//. overrride function
+	async getPermissionObject(key, organization_id) {
+		return null;
+		// try {
+		// 	if (!organization_id) {
+		// 		return null;
+		// 	}
+
+		// 	const db = this.dbClient.db(organization_id)
+		// 	if (!db)  {
+		// 		return null;
+		// 	}
+		// 	const collection = db.collection('permissions');
+		// 	if (!collection) {
+		// 		return null;
+		// 	}
+
+		// 	let permission = await collection.findOne({
+		// 		apikey: key,
+		// 		type: 'apikey'
+		// 	});
+			
+		// 	if (!permission.collections) {
+		// 		permission.collections = {};
+		// 	}
+
+		// 	if (permission && permission.roles) {
+		// 		const role_ids = permission.roles.map((x) => ObjectID(x));
+
+		// 		let roles = await collection.find({
+		// 			_id: { $in: role_ids }
+		// 		}).toArray()
+
+		// 		roles.map(role =>  {
+		// 			if (role.collections) {
+		// 				// role_collections = { ...role_collections, ...role.collections}
+		// 				for (const c in role.collections) {
+		// 					if (permission.collections[c]) {
+		// 						permission.collections[c] = [...new Set([...permission.collections[c], ...role.collections[c]])]
+		// 					}
+		// 				}
+		// 			}
+		// 		})
+		// 	}
+		// 	console.log(permission)
+			
+		// 	return permission;
+		// } catch (error) {
+		// 	return null;
+		// }
 		
 	}
 	
@@ -81,23 +131,27 @@ class CoCreatePermission {
 		}
 		
 		const { apikey, organization_id, key, key_value, type } = this.getParameters(action, data)
-		
 		const permission = await this.getRolesByKey(apikey, organization_id)
-		
-		console.log(apikey, organization_id, key, key_value, type)
+		console.log(permission)
+		console.log({apikey, organization_id, key, key_value, type})
+
 		return true;
 		
 		if (action == 'connect') return true;
+		
+		if (!permission) return false;
 		
 		if (!apikey || !organization_id ) {
 			return false;
 		}
 		
+		if (permission.super_admin) return true;
+		
 		if (permission.organization_id !== organization_id) {
 			return false;
 		}
 		
-		if (!permission.hosts || this.checkValue(permission.hosts, host)) {
+		if (!permission.hosts || !this.checkValue(permission.hosts, host)) {
 			return false;
 		}
 		
@@ -108,101 +162,53 @@ class CoCreatePermission {
 		return status;
 	}
 	
-	async getPermissionOfapikey(key, organization_id) {
-		
-		try {
-			if (!organization_id) {
-				return null;
-			}
 
-			const db = this.dbClient.db(organization_id)
-			if (!db)  {
-				return null;
-			}
-			const collection = db.collection('permissions');
-			if (!collection) {
-				return null;
-			}
-
-			let permission = await collection.findOne({
-				apikey: key,
-				type: 'apikey'
-			});
-			
-			if (!permission.collections) {
-				permission.collections = {};
-			}
-
-			if (permission && permission.roles) {
-				const role_ids = permission.roles.map((x) => ObjectID(x));
-
-				let roles = await collection.find({
-					_id: { $in: role_ids }
-				}).toArray()
-
-				roles.map(role =>  {
-					if (role.collections) {
-						// role_collections = { ...role_collections, ...role.collections}
-						for (const c in role.collections) {
-							if (permission.collections[c]) {
-								permission.collections[c] = [...new Set([...permission.collections[c], ...role_collections[c]])]
-							}
-						}
-					}
-				})
-			}
-			console.log(permission)
-			
-			return permission;
-		} catch (error) {
-			return null;
-		}
-		
-	}
 	
-	async checkByKey(action, { apiKey, organization_id, securityKey, collection, ...rest }, {headers}) {
-		const origin =  headers['origin']
-		let host = headers['x-forwarded-for'];
-		if (origin) {
-			host = url.parse(origin).hostname;
-		}
+	// async checkByKey(action, { apiKey, organization_id, securityKey, collection, ...rest }, {headers}) {
+	// 	const origin =  headers['origin']
+	// 	let host = headers['x-forwarded-for'];
+	// 	if (origin) {
+	// 		host = url.parse(origin).hostname;
+	// 	}
 
-		const permission = await this.getRolesByKey(apiKey, organization_id)
-		// console.log(permission)
-		return true;
+	// 	const permission = await this.getRolesByKey(apiKey, organization_id)
+	// 	// console.log(permission)
+	// 	return true;
 
 
-		if (action == 'connect') {
-			return true;
-		}
+	// 	if (action == 'connect') {
+	// 		return true;
+	// 	}
 		
-		if (!apiKey || !organization_id ) {
-			return false;
-		}
+	// 	if (!apiKey || !organization_id ) {
+	// 		return false;
+	// 	}
 		
-		if (!permission) {
-			return false;
-		}
+	// 	if (!permission) {
+	// 		return false;
+	// 	}
 		
-		if (permission.organization_id != organization_id) {
-			return false;
-		}
+	// 	if (permission.organization_id != organization_id) {
+	// 		return false;
+	// 	}
 		
-		if (!permission.hosts || this.checkValue(permission.hosts, host)) {
-			return false;
-		}
+	// 	if (!permission.hosts || this.checkValue(permission.hosts, host)) {
+	// 		return false;
+	// 	}
 		
-		if (!permission.collections) return false
+	// 	if (!permission.collections) return false
 
-		let status = this.checkCollection(permission.collections, collection, action)
-		if (!status) {
-			status = this.checkCollection(permission.role_collections, collection, action);
-		}
-		return status;
-	}
+	// 	let status = this.checkCollection(permission.collections, collection, action)
+	// 	if (!status) {
+	// 		status = this.checkCollection(permission.role_collections, collection, action);
+	// 	}
+	// 	return status;
+	// }
 	
 	checkCollection(collections, selected_collection, action) 
 	{
+		console.log({collections, selected_collection, action})
+		
 		if (!collections) return false;
 		
 		let collection_roles = collections[selected_collection];
